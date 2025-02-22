9dee2a1027f53764435c969d5467e83400c4b060
HughODwyer90 <56845588+HughODwyer90@users.noreply.github.com>
2025-02-22 16:43:25 +0000

    Update html_generator.py

--- python_scripts/html_generator.py
+++ python_scripts/html_generator.py
@@ -24,8 +24,8 @@ class HTMLGenerator:
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>File Dashboard</title>
-            <link rel="stylesheet" href="/local/table-styles.css">
-            <script defer src="/local/table-functions.js"></script>
+            <link rel="stylesheet" href="/assets/table-styles.css">
+            <script defer src="/assets/table-functions.js"></script>
             <script>
                 function loadContent(file) {{
                     document.getElementById('content-frame').src = file;
@@ -74,8 +74,8 @@ class HTMLGenerator:
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Home Assistant Entities</title>
-            <link rel="stylesheet" href="/local/table-styles.css">
-            <script defer src="/local/table-functions.js"></script>
+            <link rel="stylesheet" href="/assets/table-styles.css">
+            <script defer src="/assets/table-functions.js"></script>
         </head>
         <body>
             <h1>Home Assistant Entities</h1>
@@ -124,8 +124,8 @@ class HTMLGenerator:
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Home Assistant Integrations</title>
-            <link rel="stylesheet" href="/local/table-styles.css">
-            <script defer src="/local/table-functions.js"></script>
+            <link rel="stylesheet" href="/assets/table-styles.css">
+            <script defer src="/assets/table-functions.js"></script>
         </head>
         <body>
             <h1>Home Assistant Integrations</h1>
